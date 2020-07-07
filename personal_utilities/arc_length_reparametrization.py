e547bc3ca5a9827f695ac37a03c71291523b35ce
David Stein <dstein@flatironinstitute.org>
2020-07-07 14:16:06 -0400

    fiddles with arc_length_parameterize

--- personal_utilities/arc_length_reparametrization.py
+++ personal_utilities/arc_length_reparametrization.py
@@ -3,8 +3,9 @@ import fast_interp
 import scipy as sp
 import scipy.integrate
 
-from .nufft_interpolation import nufft_interpolation1d
-from .newton_solvers import VectorNewton
+from personal_utilities.nufft_interpolation import nufft_interpolation1d
+from personal_utilities.newton_solvers import VectorNewton
+from personal_utilities.fourier_filters import SimpleFourierFilter
 
 def _setup(n):
     dt = 2*np.pi/n
@@ -27,12 +28,12 @@ def _get_speed(x, y, ik):
 def _null_filter_function(f):
     return f
 
-def fractional_fourier_filter(f, fraction=2.0/3):
+def fractional_fourier_filter(f, fraction):
     fh = np.fft.rfft(f)
     fh[int(fraction*fh.shape[0]):] = 0.0
     return np.fft.irfft(fh)
 
-def arc_length_parameterize(x, y, tol=1e-14, filter_function=None, return_t=False):
+def old_arc_length_parameterize(x, y, tol=1e-14, filter_function=None, return_t=False):
     """
     Reparametrize the periodic curve defined by (x, y)
 
@@ -46,6 +47,8 @@ def arc_length_parameterize(x, y, tol=1e-14, filter_function=None, return_t=Fals
     high-frequency noise in the reparametrized (x, y)
     """
     if filter_function is None: filter_function = _null_filter_function
+    if type(filter_function) is float:
+        filter_function = SimpleFourierFilter()
     n = x.shape[0]
     dt, tv, ik, ikr = _setup(n)
     xh, yh, sd = _get_speed(x, y, ik)
@@ -72,4 +75,50 @@ def arc_length_parameterize(x, y, tol=1e-14, filter_function=None, return_t=Fals
         return filter_function(x), filter_function(y), snew
     else:
         return filter_function(x), filter_function(y)
+
+def arc_length_parameterize(x, y, tol=1e-14, filter_fraction=0.9, return_t=False):
+    """
+    Reparametrize the periodic curve defined by (x, y)
+
+    Note that although this will solve the Newton problem to tol, the actual
+    parameterization will not be arclength to that value unless the speed of
+    the parameterization is resolved to that tolerance!  Resolving the speed
+    may take significantly more resolution than the coordinates (x, y)
+    """
+    n = x.shape[0]
+    dt, tv, ik, ikr = _setup(n)
+    xh, yh, sd = _get_speed(x, y, ik)
+    # total arc-length
+    al = np.sum(sd)*dt
+    # rescaled speed
+    asd = sd*2*np.pi/al
+    # fourier transform for speed and periodized arc-length
+    pal_hat = np.fft.fft(asd)/ikr
+    sd_hat = np.fft.fft(asd)
+    # functions for newton solve to get arc-length coordinates
+    def al_func(s):
+        f = nufft_interpolation1d(s, pal_hat)
+        return f + s - tv
+    def J_func(s):
+        return nufft_interpolation1d(s, sd_hat)
+    # run Newton solver
+    solver = VectorNewton(al_func, J_func)  
+    snew = solver.solve(tv, tol=tol, verbose=False)
+    if True:
+        x = nufft_interpolation1d(snew, xh)
+        y = nufft_interpolation1d(snew, yh)
+        filt = lambda x: fractional_fourier_filter(x, filter_fraction)
+        if return_t:
+            return filt(x), filt(y), snew
+        else:
+            return filt(x), filt(y)
+    else:
+        snew = tv+fractional_fourier_filter(snew-tv, filter_fraction)
+        x = nufft_interpolation1d(snew, xh)
+        y = nufft_interpolation1d(snew, yh)
+        if return_t:
+            return x, y, snew
+        else:
+            return x, y
+
 
