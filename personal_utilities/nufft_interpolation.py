8fa7e24a74ff872c77a645f8f93d856393259c3a
David Stein <dstein@flatironinstitute.org>
2023-11-17 16:26:23 -0500

    removed finufft dependency

--- personal_utilities/nufft_interpolation.py
+++ personal_utilities/nufft_interpolation.py
@@ -1,10 +1,5 @@
 import numpy as np
-try:
-    import finufftpy
-    old_nufft = True
-except:
-    import finufft
-    old_nufft = False
+from near_finder.nufft_interp import periodic_interp1d
 
 from .transformations import affine_transformation
 
@@ -16,10 +11,13 @@ def nufft_interpolation1d(x_out, in_hat, transformer=None):
 	"""
 	if transformer is not None:
 		x_out = transformer(x_out)
-	out = np.zeros(x_out.shape[0], dtype=complex)
-	if old_nufft:
-		finufftpy.nufft1d2(x_out, out, 1, 1e-15, in_hat, modeord=1)
-	else:
-		finufft.nufft1d2(x_out, in_hat, out, isign=1, eps=1e-15, modeord=1)
-	adj = 1.0/in_hat.shape[0]
-	return out.real*adj
+	interpolater = periodic_interp1d(fh=in_hat, eps=1e-14)
+	return interpolater(x_out).real
+
+	# out = np.zeros(x_out.shape[0], dtype=complex)
+	# if old_nufft:
+	# 	finufftpy.nufft1d2(x_out, out, 1, 1e-15, in_hat, modeord=1)
+	# else:
+	# 	finufft.nufft1d2(x_out, in_hat, out, isign=1, eps=1e-15, modeord=1)
+	# adj = 1.0/in_hat.shape[0]
+	# return out.real*adj
